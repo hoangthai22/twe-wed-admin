ca89bada107d01fb9a57ce3e8207470c285022eb
HoangMinhFpt <65846831+HoangMinhFpt@users.noreply.github.com>
2022-02-23 15:55:08 +0700

    Add coffe web

--- src/layouts/dashboard/components/Projects/data/index.js
+++ src/layouts/dashboard/components/Projects/data/index.js
@@ -6,15 +6,9 @@ import Tooltip from "@mui/material/Tooltip";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import MDAvatar from "components/MDAvatar";
-import MDProgress from "components/MDProgress";
+import MDBadge from "components/MDBadge";
 
 // Images
-import logoXD from "assets/images/small-logos/logo-xd.svg";
-import logoAtlassian from "assets/images/small-logos/logo-atlassian.svg";
-import logoSlack from "assets/images/small-logos/logo-slack.svg";
-import logoSpotify from "assets/images/small-logos/logo-spotify.svg";
-import logoJira from "assets/images/small-logos/logo-jira.svg";
-import logoInvesion from "assets/images/small-logos/logo-invision.svg";
 import team1 from "assets/images/team-1.jpg";
 import team2 from "assets/images/team-2.jpg";
 import team3 from "assets/images/team-3.jpg";
@@ -46,7 +40,7 @@ export default function data() {
       </Tooltip>
     ));
 
-  const Company = ({ image, name }) => (
+  const Group = ({ image, name }) => (
     <MDBox display="flex" alignItems="center" lineHeight={1}>
       <MDAvatar src={image} name={name} size="sm" />
       <MDTypography variant="button" fontWeight="medium" ml={1} lineHeight={1}>
@@ -55,17 +49,30 @@ export default function data() {
     </MDBox>
   );
 
+  const Mentor = ({ image, name, email }) => (
+    <MDBox display="flex" alignItems="center" lineHeight={1}>
+      <MDAvatar src={image} name={name} size="sm" />
+      <MDBox ml={2} lineHeight={1}>
+        <MDTypography display="block" variant="button" fontWeight="medium">
+          {name}
+        </MDTypography>
+        <MDTypography variant="caption">{email}</MDTypography>
+      </MDBox>
+    </MDBox>
+  );
+
   return {
     columns: [
-      { Header: "companies", accessor: "companies", width: "45%", align: "left" },
+      { Header: "group", accessor: "group", width: "25%", align: "left" },
       { Header: "members", accessor: "members", width: "10%", align: "left" },
-      { Header: "budget", accessor: "budget", align: "center" },
-      { Header: "completion", accessor: "completion", align: "center" },
+      { Header: "mentor", accessor: "mentor", align: "center" },
+      { Header: "date/time", accessor: "date_time", align: "center" },
+      { Header: "status", accessor: "status", align: "center" },
     ],
 
     rows: [
       {
-        companies: <Company image={logoXD} name="Material UI XD Version" />,
+        group: <Group image={team2} name="abc" />,
         members: (
           <MDBox display="flex" py={1}>
             {avatars([
@@ -76,118 +83,135 @@ export default function data() {
             ])}
           </MDBox>
         ),
-        budget: (
-          <MDTypography variant="caption" color="text" fontWeight="medium">
-            $14,000
+        mentor: <Mentor image={team2} name="abc" email="john@creative-tim.com" />,
+        date_time: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            11/02/22__7h:45
           </MDTypography>
         ),
-        completion: (
-          <MDBox width="8rem" textAlign="left">
-            <MDProgress value={60} color="info" variant="gradient" label={false} />
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="Done" color="success" variant="gradient" size="sm" />
           </MDBox>
         ),
       },
       {
-        companies: <Company image={logoAtlassian} name="Add Progress Track" />,
+        group: <Group image={team2} name="abc" />,
         members: (
           <MDBox display="flex" py={1}>
             {avatars([
+              [team1, "Ryan Tompson"],
               [team2, "Romina Hadid"],
+              [team3, "Alexander Smith"],
               [team4, "Jessica Doe"],
             ])}
           </MDBox>
         ),
-        budget: (
-          <MDTypography variant="caption" color="text" fontWeight="medium">
-            $3,000
+        mentor: <Mentor image={team2} name="abc" email="john@creative-tim.com" />,
+        date_time: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            11/02/22__7h:45
           </MDTypography>
         ),
-        completion: (
-          <MDBox width="8rem" textAlign="left">
-            <MDProgress value={10} color="info" variant="gradient" label={false} />
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="Done" color="success" variant="gradient" size="sm" />
           </MDBox>
         ),
       },
       {
-        companies: <Company image={logoSlack} name="Fix Platform Errors" />,
+        group: <Group image={team2} name="abc" />,
         members: (
           <MDBox display="flex" py={1}>
             {avatars([
               [team1, "Ryan Tompson"],
-              [team3, "Alexander Smith"],
-            ])}
-          </MDBox>
-        ),
-        budget: (
-          <MDTypography variant="caption" color="text" fontWeight="medium">
-            Not set
-          </MDTypography>
-        ),
-        completion: (
-          <MDBox width="8rem" textAlign="left">
-            <MDProgress value={100} color="success" variant="gradient" label={false} />
-          </MDBox>
-        ),
-      },
-      {
-        companies: <Company image={logoSpotify} name="Launch our Mobile App" />,
-        members: (
-          <MDBox display="flex" py={1}>
-            {avatars([
-              [team4, "Jessica Doe"],
-              [team3, "Alexander Smith"],
               [team2, "Romina Hadid"],
-              [team1, "Ryan Tompson"],
-            ])}
-          </MDBox>
-        ),
-        budget: (
-          <MDTypography variant="caption" color="text" fontWeight="medium">
-            $20,500
-          </MDTypography>
-        ),
-        completion: (
-          <MDBox width="8rem" textAlign="left">
-            <MDProgress value={100} color="success" variant="gradient" label={false} />
-          </MDBox>
-        ),
-      },
-      {
-        companies: <Company image={logoJira} name="Add the New Pricing Page" />,
-        members: (
-          <MDBox display="flex" py={1}>
-            {avatars([[team4, "Jessica Doe"]])}
-          </MDBox>
-        ),
-        budget: (
-          <MDTypography variant="caption" color="text" fontWeight="medium">
-            $500
-          </MDTypography>
-        ),
-        completion: (
-          <MDBox width="8rem" textAlign="left">
-            <MDProgress value={25} color="info" variant="gradient" label={false} />
-          </MDBox>
-        ),
-      },
-      {
-        companies: <Company image={logoInvesion} name="Redesign New Online Shop" />,
-        members: (
-          <MDBox display="flex" py={1}>
-            {avatars([
-              [team1, "Ryan Tompson"],
+              [team3, "Alexander Smith"],
               [team4, "Jessica Doe"],
             ])}
           </MDBox>
         ),
-        budget: (
-          <MDTypography variant="caption" color="text" fontWeight="medium">
-            $2,000
+        mentor: <Mentor image={team2} name="abc" email="john@creative-tim.com" />,
+        date_time: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            11/02/22__7h:45
           </MDTypography>
         ),
-        completion: (
-          <MDBox width="8rem" textAlign="left">
-            <MDProgress value={40} color="info" variant="gradient" label={false} />
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="Done" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+      },
+      {
+        group: <Group image={team2} name="abc" />,
+        members: (
+          <MDBox display="flex" py={1}>
+            {avatars([
+              [team1, "Ryan Tompson"],
+              [team2, "Romina Hadid"],
+              [team3, "Alexander Smith"],
+              [team4, "Jessica Doe"],
+            ])}
+          </MDBox>
+        ),
+        mentor: <Mentor image={team2} name="abc" email="john@creative-tim.com" />,
+        date_time: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            11/02/22__7h:45
+          </MDTypography>
+        ),
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="Done" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+      },
+      {
+        group: <Group image={team2} name="abc" />,
+        members: (
+          <MDBox display="flex" py={1}>
+            {avatars([
+              [team1, "Ryan Tompson"],
+              [team2, "Romina Hadid"],
+              [team3, "Alexander Smith"],
+              [team4, "Jessica Doe"],
+            ])}
+          </MDBox>
+        ),
+        mentor: <Mentor image={team2} name="abc" email="john@creative-tim.com" />,
+        date_time: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            11/02/22__7h:45
+          </MDTypography>
+        ),
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="Done" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+      },
+      {
+        group: <Group image={team2} name="abc" />,
+        members: (
+          <MDBox display="flex" py={1}>
+            {avatars([
+              [team1, "Ryan Tompson"],
+              [team2, "Romina Hadid"],
+              [team3, "Alexander Smith"],
+              [team4, "Jessica Doe"],
+            ])}
+          </MDBox>
+        ),
+        mentor: <Mentor image={team2} name="abc" email="john@creative-tim.com" />,
+        date_time: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            11/02/22__7h:45
+          </MDTypography>
+        ),
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="Done" color="success" variant="gradient" size="sm" />
           </MDBox>
         ),
       },
